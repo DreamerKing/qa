6eaad507e168831ed7302cae4353fe972f4d00e7
wangxiancao <wangxianchao@zgyjyx.com>
2017-06-26 13:38:17 +0800

    v2.1.4

--- public/assets/lib/ckeditor/config.js
+++
@@ -1,6 +0,0 @@
-
-CKEDITOR.editorConfig = function( config ) {
-	config.language = 'zh-cn';
-	// config.uiColor = '#AADC6E';
-  config.extraPlugins += (config.extraPlugins ? ',FMathEditor': 'FMathEditor');
-};
